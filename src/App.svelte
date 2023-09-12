fb5d6f198bc376191c4c8c8b7e02f21675791bc6
Jeremy Helsel <JIHlucky7@gmail.com>
2023-09-12 13:46:26 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Logo from './assets/logo.svg';
+    import Body from './lib/Body.svelte';
+</script>
+
+<main>
+    <h1><img src={Logo} alt="logo"></h1>
+    <Body></Body>
+</main>
+
+<style>
+</style>
---
+++ src/lib/ButtonPanel.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<div>
+    <button>5%</button>
+    <button>10%</button>
+    <button>15%</button>
+    <button>25%</button>
+    <button>50%</button>
+    <button>Custom</button>
+</div>
